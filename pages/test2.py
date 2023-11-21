a9ec5869f5d5139f9cdaed67021e5236cff2346d
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-11-21 00:59:08 -0600

    Update test2.py

--- pages/test2.py
+++ pages/test2.py
@@ -6,15 +6,15 @@ import os
 from io import BytesIO
 import base64
 
-URL_WEBHOOK = "https://hooks.zapier.com/hooks/catch/17095058/3krt46r/"
 
-uploaded_img = st.file_uploader("Upload a picture of the person/pet you'd like on a card, board, or coloring page", type=['png', 'jpg', 'jpeg', 'gif'])
+
+uploaded_img1 = st.file_uploader("Upload a picture of the person/pet you'd like on a card, board, or coloring page", type=['png', 'jpg', 'jpeg', 'gif'])
 
 # Check if an image has been uploaded
-if uploaded_img is not None:
+if uploaded_img1 is not None:
     # To read image file buffer with PIL:
     from PIL import Image
-    image = Image.open(uploaded_img)
+    image = Image.open(uploaded_img1)
 
     # Display the uploaded image
     st.image(image, caption='Uploaded Image.', use_column_width=True)
