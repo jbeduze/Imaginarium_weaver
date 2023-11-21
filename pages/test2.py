0a9b29a47b30be8df2500d2181faf21d52e9c514
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-11-21 01:05:48 -0600

    Update test2.py

--- pages/test2.py
+++ pages/test2.py
@@ -6,6 +6,8 @@ import os
 from io import BytesIO
 import base64
 
+b="./app/static/tempimage.jpg"
+image_url = "https://imaginariumweaver.streamlit.app/app/static/tempimage.jpg"
 
 
 uploaded_img1 = st.file_uploader("Upload a picture of the person/pet you'd like on a card, board, or coloring page", type=['png', 'jpg', 'jpeg', 'gif'])
@@ -18,3 +20,9 @@ if uploaded_img1 is not None:
 
     # Display the uploaded image
     st.image(image, caption='Uploaded Image.', use_column_width=True)
+
+with open(b, "wb") as f:
+    uploaded_img1.seek(0)
+    f.write(uploaded_img.read())
+    st.markdown(f"[![Click to view uploaded image]({image_url})]({image_url})")
+    
