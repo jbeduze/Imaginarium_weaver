bc6836675ea171709968768d08a4522afb0cd12b
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2023-11-20 20:28:06 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,1 +1,10 @@
 import streamlit as st
+from st_paywall import add_auth
+
+st.title("Welcome to Daddy Bets")
+
+add_auth(required=True)
+
+st.write(f"Subscription Status: {st.session_state.user_subscribed}")
+st.write("congrats on your future winnings now get in there champ")
+st.write(f'for your records and future logins, your email is: {st.session_state.email}')
